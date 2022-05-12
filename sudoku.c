40b4ed7602e1c29a5c13f802cbefac59cb77f558
benjadiazu <>
2022-05-12 15:22:37 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -52,12 +52,12 @@ int is_valid(Node* n){
 List* get_adj_nodes(Node* n){
     List* list=createList();
 
-
+    int i,j,k;
     //MATRIZ
-    for (int i = 0 ; i < 3 ; i++){
-        for (int j = 0 ; j < 3 ; j++){
+    for ( i = 0 ; i < 3 ; i++){
+        for ( j = 0 ; j < 3 ; j++){
             if (n->sudo[i][j] == 0){
-                for (int k = 1 ; k < 10 ; k++){
+                for ( k = 1 ; k < 10 ; k++){
                     n->sudo[i][j] = k;
                     if (is_valid(n)){
                         Node *adj = copy(n);
