218108c4900fc54b0ea5bb4f1e0337c39cdd9d2b
benjadiazu <>
2022-05-14 19:05:53 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -51,7 +51,8 @@ int is_valid(Node* n){
     for (int i = 0 ; i < 9 ; i++){
         for (int j = 0 ; j < 9 ; j++){
             int posicion = n->sudo[i][j];
-            if (vector[posicion] != 0){
+            if (vector[posicion] == 0){
+                printf("Entra :_ B\n");
                 vector[posicion] = 1;
             }
             else {printf("Entra\n");return 0;}
