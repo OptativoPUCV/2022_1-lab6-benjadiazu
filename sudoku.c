152419b192fb07ab0403b2d92b42adb4dfa3cc3c
benjadiazu <>
2022-05-15 01:55:10 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -139,7 +139,7 @@ Node* DFS(Node* initial, int* cont){
   return NULL;
 }
 
-
+/*
 int main( int argc, char *argv[] ){
 
   Node* initial= read_file("s12a.txt");;
@@ -151,3 +151,4 @@ int main( int argc, char *argv[] ){
 
   return 0;
 }
+*/
